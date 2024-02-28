c4e23facbed943c915e2445f064ed1909a918b2c
orianachv <chacon.oriana.19@gmail.com>
2024-02-28 11:46:44 +0100

    wip adding hook to verify screen size and  working in header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Experience from './components/Experience';
+import useScreenSize from './hooks/useScreenSize';
 
 function App() {
+  const screenSize = useScreenSize();
+
   return (
-    <div className="App flex flex-col md:flex-row h-screen">
+    <div className={`flex flex-col md:flex-row ${screenSize === 'desktop' ? 'h-screen' : ''}`}>
       <div className="bg-gray-800 text-white overflow-auto py-8 md:w-1/3 lg:w-1/4 xl:w-1/5 h-full">
-        {/* Menú lateral que se ajustará basado en el tamaño de la pantalla */}
         <Header />
       </div>
       <div className="flex-1 overflow-auto py-8 px-4">
-        {/* Contenido principal que se ajustará basado en el tamaño de la pantalla */}
         <About />
         <Experience />
         <Skills />
@@ -23,4 +24,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,75 +1,46 @@
 import React, { useState } from 'react';
 
+// Componente para los enlaces del header, reutilizable
+const HeaderLinks: React.FC<{ cvUrl: string; onLinkClick: () => void }> = ({ cvUrl, onLinkClick }) => (
+    <div className="flex flex-col md:items-center space-y-2 md:space-y-0 md:space-x-4">
+        <a href="#about" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">About</a>
+        <a href="#experience" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Experience</a>
+        <a href="#skills" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Skills</a>
+        <a href="#contact" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Contact</a>
+        <a href={cvUrl} download="Oriana_Chacon_Resume.pdf" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Resume</a>
+    </div>
+);
+
 const Header: React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const cvUrl = 'https://github.com/orianachv/resume/raw/main/OrianaChaconResume.pdf';
 
+    // Función para manejar el cierre del menú en dispositivos móviles
+    const handleLinkClick = () => setIsMenuOpen(false);
+
     return (
-        <header className="bg-gradient-to-r from-teal-400 to-blue-600">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-16">
-              <div className="flex items-center">
-                <h1 className="text-white text-2xl font-bold md:text-3xl">
-                  Oriana Chacon Vivas
-                </h1>
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <a href="#about" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                    About
-                  </a>
-                  <a href="#experience" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                    Experience
-                  </a>
-                  <a href="#skills" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                    Skills
-                  </a>
-                  <a href="#contact" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                    Contact
-                  </a>
-                  <a href="/path/to/resume.pdf" download="Oriana_Chacon_Resume.pdf" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                    Resume
-                  </a>
-                </div>
-              </div>
-              <div className="md:hidden">
-                <button onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="bg-teal-500 inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-500 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                  <span className="sr-only">Open main menu</span>
-                  {!isMenuOpen ? (
-                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+        <header className="py-4 flex flex-col md:flex-row">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex md:flex-col justify-between w-full">
+                <h1 className="text-white text-2xl font-bold md:text-3xl md:mb-8">Oriana Chacon Vivas</h1>
+                <button onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="md:hidden bg-teal-500 p-2 rounded-md text-white hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-500 focus:ring-white">
+                    <svg className={`h-6 w-6 ${isMenuOpen ? 'hidden' : 'block'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                     </svg>
-                  ) : (
-                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                    <svg className={`h-6 w-6 ${isMenuOpen ? 'block' : 'hidden'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
-                  )}
                 </button>
-              </div>
+                <div className="hidden md:block ">
+                    <HeaderLinks cvUrl={cvUrl} onLinkClick={() => {}} />
+                </div>
             </div>
-          </div>
-    
-          <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden`}>
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <a href="#about" className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                About
-              </a>
-              <a href="#experience" className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                Experience
-              </a>
-              <a href="#skills" className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                Skills
-              </a>
-              <a href="#contact" className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                Contact
-              </a>
-              <a href={cvUrl} download="Oriana_Chacon_Resume.pdf" className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-teal-500 transition duration-150 ease-in-out">
-                Resume
-              </a>
-            </div>
-          </div>
+            {isMenuOpen && (
+                <div className="md:hidden px-8 py-4">
+                    <HeaderLinks cvUrl={cvUrl} onLinkClick={handleLinkClick} />
+                </div>
+            )}
         </header>
-      );
+    );
 };
 
 export default Header;
