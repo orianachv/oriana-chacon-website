34f14f8e051d58bb3c215d51434b67fc5feb7e97
orianachv <chacon.oriana.19@gmail.com>
2024-03-07 17:02:48 +0100

    adding social media icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,11 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Experience from './components/Experience';
-import useScreenSize from './hooks/useScreenSize';
 
 function App() {
-  const screenSize = useScreenSize();
-
   return (
-    <div className={`flex flex-col md:flex-row ${screenSize === 'desktop' ? 'h-screen' : ''}`}>
-      <div className="bg-gray-800 text-white overflow-auto py-8 md:w-1/3 lg:w-1/4 xl:w-1/5 h-full">
+    <div className="flex flex-col md:flex-row md:h-screen">
+      <div className="bg-gray-800 text-white overflow-auto py-8 md:w-1/4 lg:w-1/4 xl:w-1/4 h-full">
         <Header />
       </div>
       <div className="flex-1 overflow-auto py-8 px-4">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,71 @@
 import React, { useState } from 'react';
+import github from '../icons/github.png'
+import linkedin from '../icons/linkedin.png'
+import gmail from '../icons/gmail-dos.png'
+import close from '../icons/close.png'
+import menu from '../icons/menu.png'
 
-// Componente para los enlaces del header, reutilizable
+
+const SOCIAL_LINKS = {
+  linkedin: 'https://www.linkedin.com/in/oriana-chacon-vivas-0823b7100/',
+  github: 'https://github.com/orianachv',
+  gmail: 'mailto:chacon.oriana.19@gmail.com'
+};
+
 const HeaderLinks: React.FC<{ cvUrl: string; onLinkClick: () => void }> = ({ cvUrl, onLinkClick }) => (
-    <div className="flex flex-col md:items-center space-y-2 md:space-y-0 md:space-x-4">
-        <a href="#about" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">About</a>
-        <a href="#experience" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Experience</a>
-        <a href="#skills" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Skills</a>
-        <a href="#contact" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Contact</a>
-        <a href={cvUrl} download="Oriana_Chacon_Resume.pdf" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Resume</a>
-    </div>
+  <div className="flex flex-col items-center space-y-2 md:space-y-0 md:space-x-4">
+    <a href="#about" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">About</a>
+    <a href="#experience" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Experience</a>
+    <a href="#skills" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Skills</a>
+    <a href="#contact" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Contact</a>
+    <a href={cvUrl} download="Oriana_Chacon_Resume.pdf" onClick={onLinkClick} className="text-white hover:text-gray-200 transition duration-150 ease-in-out">Resume</a>
+  </div>
 );
 
+const SocialLinks: React.FC = () => (
+  <div className='flex flex-row gap-3 justify-center mt-8'>
+    <a href={SOCIAL_LINKS.linkedin}>
+      <img alt='social-media' src={linkedin} className='w-6 h-6' />
+
+    </a>
+    <a href={SOCIAL_LINKS.github}>
+      <img alt='social-media' src={github} className='w-6 h-6' />
+
+    </a>
+    <a href={SOCIAL_LINKS.gmail}>
+      <img alt='social-media' src={gmail} className='w-6 h-6' />
+    </a>
+  </div>
+)
+
 const Header: React.FC = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const cvUrl = 'https://github.com/orianachv/resume/raw/main/OrianaChaconResume.pdf';
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const cvUrl = 'https://github.com/orianachv/resume/raw/main/CV%20ENGLISH%20ORIANA%20CHACO%CC%81N%20%20(1).pdf';
 
-    // Función para manejar el cierre del menú en dispositivos móviles
-    const handleLinkClick = () => setIsMenuOpen(false);
+  const handleLinkClick = () => setIsMenuOpen(false);
 
-    return (
-        <header className="py-4 flex flex-col md:flex-row">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex md:flex-col justify-between w-full">
-                <h1 className="text-white text-2xl font-bold md:text-3xl md:mb-8">Oriana Chacon Vivas</h1>
-                <button onClick={() => setIsMenuOpen(!isMenuOpen)} type="button" className="md:hidden bg-teal-500 p-2 rounded-md text-white hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-500 focus:ring-white">
-                    <svg className={`h-6 w-6 ${isMenuOpen ? 'hidden' : 'block'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-                    </svg>
-                    <svg className={`h-6 w-6 ${isMenuOpen ? 'block' : 'hidden'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-                <div className="hidden md:block ">
-                    <HeaderLinks cvUrl={cvUrl} onLinkClick={() => {}} />
-                </div>
-            </div>
-            {isMenuOpen && (
-                <div className="md:hidden px-8 py-4">
-                    <HeaderLinks cvUrl={cvUrl} onLinkClick={handleLinkClick} />
-                </div>
-            )}
-        </header>
-    );
+  return (
+    <header className="py-4 flex flex-col md:flex-row">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex md:flex-col justify-between w-full">
+        <h1 className="text-white text-2xl font-bold md:text-3xl md:mb-8">Oriana Chacon Vivas</h1>
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="md:hidden focus:outline-none">
+          <img src={isMenuOpen ? close : menu} alt="Menu" className="w-6 h-6" />
+        </button>
+
+        <div className="hidden md:block ">
+          <HeaderLinks cvUrl={cvUrl} onLinkClick={() => { }} />
+
+          <SocialLinks />
+        </div>
+      </div>
+      {isMenuOpen && (
+        <div className="md:hidden px-8 py-4">
+          <HeaderLinks cvUrl={cvUrl} onLinkClick={handleLinkClick} />
+          <SocialLinks />
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
